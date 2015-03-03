fda86c8db60848519af25f503360ade855909e7f
Andy Wu <andyplusplus@gmail.com>
2015-03-03 14:25:30 -0500

    update HUS page

---
+++ honeywell-hus_files/_vti_cnf/sp.runtime.js
@@ -0,0 +1,8 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|03 Mar 2015 19:24:23 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|honeywell-hus.html
+vti_cacheddtm:TX|03 Mar 2015 19:17:28 -0000
+vti_filesize:IR|68791
+vti_modifiedby:SR|Win8PC\\andy
+vti_nexttolasttimemodified:TR|03 Mar 2015 19:17:28 -0000
